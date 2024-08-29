a25c979672f373df1e3fd50a076cca42457f53bf
packsunjun <lee5003200@naver.com>
2024-08-29 23:29:40 +0900

    Add Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Appshell from "./Appshell";
+import Home from "./Home";
+import Texts from "./Texts";
+import Words from "./Words";
 
 class App extends React.Component{
     render() {
         return(
-            <Appshell/>
+            <Router>
+                <Appshell>
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="/texts" element={<Texts />} />
+                        <Route exact path="/words" element={<Words />} />
+                    </Routes>
+                </Appshell>
+            </Router>
         )
     }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+
+class Home extends React.Component{
+    render(){
+        return(
+            <Card>
+                React 및 FireBase 기반의 워드 클라우드 애플리케이션
+            </Card>
+        )
+    }
+}
+
+export default Home;
